82826befdfb545dafb32040912d10a63fd5e14c0
Sunip-scwt <dev9.scwt@gmail.com>
2025-12-26 18:24:27 +0530

    works on subscription page

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SubscriptionPlan;
 use Illuminate\Http\Request;
 
 class SubscriptionController extends Controller
 {
     public function subscription() {
-        return view('subscription');
+        $plans=SubscriptionPlan::where('enabled',1)
+            ->get();
+        $result=compact('plans');
+        return view('subscription')->with($result);
     }
+
 }
--- resources/views/subscription.blade.php
+++ resources/views/subscription.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.layout')
 @section('content')
 <div class="container">
-    <div class="col-sm-4">
-        
+    <div class="row subscription-row">
+
+        @foreach($plans as $plan)
+        <div class="col-sm-4">
+            <div class="card subscription-card">
+                <h2>{{$plan->name}}</h2>
+                <h4>${{$plan->amount}} Charge</h4>
+                <button class="btn btn-primary">Subscribe</button>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
